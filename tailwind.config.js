b0b992083290cb5685c213ae67379c963afebbc9
M. Hanif Azhary <hanif@azhary.dev>
2021-11-10 13:34:51 +0000

    feat: add Inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'body': ['Inter', 'system-ui', 'sans-serif'],
+      // 'body': ['Inter', 'system-ui', 'sans-serif'],
+    },
     extend: {},
   },
   variants: {
