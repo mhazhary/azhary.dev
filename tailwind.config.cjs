85d2207ad24761590b8ddf94b7600d3c3c120305
M. Hanif Azhary <hanif@azhary.dev>
2023-07-09 12:16:22 +0000

    feat: change font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		fontFamily: {
+			'display': ['Space Grotesk', ...defaultTheme.fontFamily.sans]
+		}
 	},
 	plugins: [],
 }
